f5cb859cdd1837f46557611d80e5ce00c1a23908
liushui <adcaoe@126.com>
2023-02-19 21:54:42 +0800

    localtest1

--- src/main/java/com/example/springboot2/Springboot2Application.java
+++ src/main/java/com/example/springboot2/Springboot2Application.java
@@ -11,7 +11,6 @@ public class Springboot2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Springboot2Application.class, args);
-        System.out.print("111");
     }
 
 }
